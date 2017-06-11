8610205d87beba67f14fe64f5357ed996a3d0a0c
John <iplanetcn@gmail.com>
2017-06-12 00:00:40 +0800

    enable cors access for some request mapping("/*","/customer/*")

--- src/main/java/com/phenix/study/SpringBootDockerApplication.java
+++ src/main/java/com/phenix/study/SpringBootDockerApplication.java
@@ -2,6 +2,10 @@ package com.phenix.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class SpringBootDockerApplication {
@@ -9,4 +13,23 @@ public class SpringBootDockerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDockerApplication.class, args);
     }
+
+    /**
+     * enable cors for all
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*");
+                registry.addMapping("/customer/*")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
 }
