b72eca49666d19ceda4bd7a1fe17e857b98ace74
john <iplanetcn@gmail.com>
2019-04-03 13:22:56 +0800

    support thymeleaf

--- src/main/java/com/phenix/study/controller/HomeController.java
+++ src/main/java/com/phenix/study/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.phenix.study.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.annotations.ApiIgnore;
@@ -14,11 +13,10 @@ import springfox.documentation.annotations.ApiIgnore;
  */
 @ApiIgnore
 @Controller
-public class WelcomeController {
+public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String showMessage(ModelMap model) {
-        model.put("message", " to spring boot docker!");
-        return "welcome";
+    public String showMessage() {
+        return "index";
     }
 }
